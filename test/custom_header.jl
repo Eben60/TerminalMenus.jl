6415f6d09e9482a79c143da56c4a1c63c7cc0336
Ben Elkin <8xe8n-cm@online.de>
2025-11-02 16:49:15 +0100

    minor test improvements

--- test/custom_header.jl
+++ test/custom_header.jl
@@ -20,51 +20,51 @@ function simulate_input_and_capture_output(menu::TerminalMenus.AbstractMenu, key
     out_buffer = IOBuffer()
     terminal = TerminalMenus.default_terminal(; in=new_stdin, out=out_buffer)
 
-    request(terminal, menu; kwargs...)
+    with_logger(SimpleLogger(stderr, Logging.Error)) do
+        request(terminal, menu; kwargs...)
+    end
 
     return String(take!(out_buffer))
 end
 
 @testset "Custom Header" begin
-    with_logger(SimpleLogger(stderr, Logging.Error)) do
-        header = "My Custom Header"
-        menu = RadioMenu(["a", "b", "c"]; header=header)
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K" * header)
+    header = "My Custom Header"
+    menu = RadioMenu(["a", "b", "c"]; header=header)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K" * header)
 
-        menu = MultiSelectMenu(["a", "b", "c"]; header=header)
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K" * header)
+    menu = MultiSelectMenu(["a", "b", "c"]; header=header)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K" * header)
 
-        header = ""
-        menu = RadioMenu(["1st", "b", "c"]; header=header)
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K > 1st")
+    header = ""
+    menu = RadioMenu(["1st", "b", "c"]; header=header)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K > 1st")
 
-        menu = RadioMenu(["1st", "b", "c"]; header = false)
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K > 1st")
+    menu = RadioMenu(["1st", "b", "c"]; header = false, warn=false)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K > 1st")
 
-        menu = MultiSelectMenu(["1st", "b", "c"]; header = false)
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K > [ ] 1st")
-        
-        menu = RadioMenu(["1st", "b", "c"])
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K > 1st")
-        
-        header = TerminalMenus.default_radio_header
-        menu = RadioMenu(["1st", "b", "c"]; header = true)
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K" * header)
+    menu = MultiSelectMenu(["1st", "b", "c"]; header = false)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K > [ ] 1st")
+    
+    menu = RadioMenu(["1st", "b", "c"]; warn=false)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K > 1st")
+    
+    header = TerminalMenus.default_radio_header
+    menu = RadioMenu(["1st", "b", "c"]; header = true)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K" * header)
 
-        header = TerminalMenus.default_msm_header
-        menu = MultiSelectMenu(["a", "b", "c"]; header=true)
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K" * header)
+    header = TerminalMenus.default_msm_header
+    menu = MultiSelectMenu(["a", "b", "c"]; header=true, warn=false)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K" * header)
 
-        menu = MultiSelectMenu(["a", "b", "c"])
-        output = simulate_input_and_capture_output(menu, 'q')
-        @test startswith(output, "\e[2K" * header)
-    end
+    menu = MultiSelectMenu(["a", "b", "c"]; warn=false)
+    output = simulate_input_and_capture_output(menu, 'q')
+    @test startswith(output, "\e[2K" * header)
 end
--- test/quit_selection.jl
+++ test/quit_selection.jl
@@ -1,15 +1,11 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 @testset "Quit Selection" begin
-    # Menu called without any options (may) result in a warning, but in these tests we do not check it, 
-    # just suppress warnings output, if any
-    with_logger(SimpleLogger(stderr, Logging.Error)) do
-        menu = RadioMenu(["a", "b", "c"])
-        @test simulate_input(menu, 'q') == -1
+    menu = RadioMenu(["a", "b", "c"]; warn=false)
+    @test simulate_input(menu, 'q') == -1
 
-        menu = MultiSelectMenu(["a", "b", "c"])
-        @test simulate_input(menu, 'q') == Set{Int64}()
-    end
+    menu = MultiSelectMenu(["a", "b", "c"]; warn=false)
+    @test simulate_input(menu, 'q') == Set{Int64}()
 
     menu = RadioMenu(["a", "b", "c"]; charset=:ascii)
     @test simulate_input(menu, 'q') == -1
